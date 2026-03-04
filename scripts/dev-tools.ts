eac2051050e776b517236b23adbb9c1e9e18e078
boburkhayitov <bobur.khayitov@siemens.com>
2026-03-04 11:45:26 +0100

    revert

--- scripts/dev-tools.ts
+++ scripts/dev-tools.ts
@@ -12,7 +12,7 @@ const __dirname = dirname(__filename);
 config({ path: resolve(__dirname, "../.env") });
 
 // GitHub and GitLab client init
-const githubToken = process.env.GITHUB_TOKEN;
+const githubToken = process.env.GH_TOKEN;
 if (!githubToken) {
   throw new Error("GH_TOKEN is not defined");
 }
